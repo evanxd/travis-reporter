67feca1cbf649c26a5caf3d9cdb5031d0e4c4767
John Wang <john29917958@gmail.com>
2014-04-29 11:00:59 +0800

    Issue 65 - Action item 8 - Receive user's input (detail, search tool,home).

--- resources/travis-reporter/public/javascripts/button.js
+++ resources/travis-reporter/public/javascripts/button.js
@@ -1,46 +1,54 @@
-// JavaScript Document
-/*$(document).ready(function() {
-	$('button').hover(
-		function() {
-        	$(this).addClass('bt_hover');
-		},
-		function() {
-			$(this).removeClass('bt_hover');	
+'use strict';
+define(function () {
+	function hoverAction(target) {
+		$(target).hover(
+			function() {
+				$(this).addClass('bt_hover');
+			},
+			function() {
+				$(this).removeClass('bt_hover');	
+				$(this).removeClass('bt_pressed');
+			}
+		);
+	}
+
+	function pressAction(target) {
+		$(target).mousedown(function() {
+			$(this).removeClass('bt_hover');
+			$(this).addClass('bt_pressed');
+		});
+	}
+
+	function releaseAction(target) {
+		$(target).mouseup(function() {
 			$(this).removeClass('bt_pressed');
-		}
-	);
-	
-	$('button').mousedown(function() {
-		$(this).removeClass('bt_hover');
-		$(this).addClass('bt_pressed');
-	});
-	
-	$('button').mouseup(function() {
-		$(this).removeClass('bt_pressed');
-		$(this).addClass('bt_hover');
-	});
-	
-	$('th.tb_header').hover(
-		function() {
-        	$(this).addClass('bt_hover');
+			$(this).addClass('bt_hover');
+		});
+	}
+
+	return {
+		init: function() {
+			hoverAction('button, th.tb_header');
+			pressAction('button, th.tb_header');
+			releaseAction('button, th.tb_header');
 		},
-		function() {
-			$(this).removeClass('bt_hover');	
-			$(this).removeClass('bt_pressed');
+
+		addButtonAction: function(target) {
+			if(target == null) {
+				target = $('button, th.tb_header');
+			}
+			
+			hoverAction(target);
+			pressAction(target);
+			releaseAction(target);
+		},
+
+		removeButtonAction: function(target) {
+			if(target == null) {
+				target = $('button, th.tb_header');
+			}
+
+			$(target).removeClass();
 		}
-	);
-	
-	$('th.tb_header').mousedown(function() {
-		$(this).removeClass('bt_hover');
-		$(this).addClass('bt_pressed');
-	});
-	
-	$('th.tb_header').mouseup(function() {
-		$(this).removeClass('bt_pressed');
-		$(this).addClass('bt_hover');
-	});
-	
-	$('option').onclick(function() {
-		
-	});
-});*/
+	};
+});
