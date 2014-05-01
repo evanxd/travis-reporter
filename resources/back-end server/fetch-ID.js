b67d31d022ce29fac6b4d527930617753d466784
林靖凱 <promise82114@gmail.com>
2014-05-01 22:18:45 +0800

    Issue 75 - Action item 15 - Fetch the ID from Travis-CI.

---
+++ resources/back-end server/fetch-ID.js
@@ -0,0 +1,32 @@
+var Travis = require('travis-ci');
+var travis = new Travis({
+    version: '2.0.0'
+});
+var owner = 'mozilla-b2g',
+    reponame = 'gaia';
+travis.repos({
+    owner_name: owner,
+    name: reponame
+}, function (err, res) {
+    console.log(owner+"/"+reponame);
+    console.log("repo id: "+res.repo.id);
+    var BUILD_ID = res.repo.last_build_id;
+    travis.builds({
+        id: BUILD_ID
+    }, function(err, res){
+        console.log("last build id: "+res.build.id);
+        for(var i in res.build.job_ids){
+            var JOB_ID = res.build.job_ids[i];
+            travis.jobs({
+                id: JOB_ID
+            }, function(err, res){
+                var LOG_ID = res.job.log_id;
+                travis.logs({
+                    id: LOG_ID
+              }, function(err, res){
+                    console.log("job id: "+res.log.job_id+" ; log id: "+res.log.id);
+                });
+            });
+        }
+    });
+});
