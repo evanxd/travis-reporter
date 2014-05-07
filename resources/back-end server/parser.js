a42e9e31473fa6a3c24795ff29e6bf0604fa680e
蘇翌帆 <x95102003@yahoo.com.tw>
2014-05-07 20:04:13 +0800

    change parser function to the return function and remove callback

--- resources/back-end server/parser.js
+++ resources/back-end server/parser.js
@@ -1,22 +1,11 @@
 
 
 module.exports = {
-	findErrFile : function(data,callback){
+	findErrFile : function(data){
 		var patt=/(\w)*\.js/g;
 		var spiltStr=data.substr(data.indexOf("failing"));
         spiltStr=spiltStr.substr(0,spiltStr.indexOf("make"));
        	var result =spiltStr.match(patt);
-       	if(callback&&typeof(callback)==="function")
-       		callback(result);
-       	
-	}
-
+       	return result;
+       }
 }
-
-
-// function findErrorFile(data){
-// 	var patt=/(\w)*\.js/g;
-// 	var spiltStr=data.substr(data.indexOf("failing"));
-//             spiltStr=spiltStr.substr(0,spiltStr.indexOf("make"));
-//             return spiltStr.match(patt);
-// }
