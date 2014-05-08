439965f56b077a0488f48dfa663c19783f81fe5b
Sue-Yi-FAN <x95102003@yahoo.com.tw>
2014-05-08 10:40:06 -0700

    add runServer.js which can establish a server and runTime.js can be used to call other function in every setTime.

--- resources/back-end server/parser.js
+++ resources/back-end server/parser.js
@@ -3,20 +3,11 @@
 module.exports = {
 	findErrFile : function(data,callback){
 		var patt=/(\w)*\.js/g;
+		console.log(data);
 		var spiltStr=data.substr(data.indexOf("failing"));
         spiltStr=spiltStr.substr(0,spiltStr.indexOf("make"));
        	var result =spiltStr.match(patt);
-       	if(callback&&typeof(callback)==="function")
-       		callback(result);
-       	
+        return result;
 	}
 
 }
-
-
-// function findErrorFile(data){
-// 	var patt=/(\w)*\.js/g;
-// 	var spiltStr=data.substr(data.indexOf("failing"));
-//             spiltStr=spiltStr.substr(0,spiltStr.indexOf("make"));
-//             return spiltStr.match(patt);
-// }
---
+++ resources/back-end server/runTimer.js
@@ -0,0 +1,8 @@
+var fetch=require('./fetch-ID');
+var parser=require('./parser')
+var addr='https://s3.amazonaws.com/archive.travis-ci.org/jobs/23735955/log.txt';
+exports.inter = setInterval(function(){
+	
+},2000);
+function printErr(){
+}
