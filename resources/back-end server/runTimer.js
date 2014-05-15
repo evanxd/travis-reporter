dd17a94a894855e4da94e3d7fa03e79005659c92
Sue-Yi-FAN <x95102003@yahoo.com.tw>
2014-05-14 21:51:46 -0700

    change folder name and create makefile to run modify back-end-server js file and let it could show some fake result

--- resources/back-end server/runTimer.js
+++
@@ -1,6 +0,0 @@
-//var parser=require('./parser')
-//var addr='https://s3.amazonaws.com/archive.travis-ci.org/jobs/23735955/log.txt';
-exports.inter = setInterval(function(){
-	//printErr('hi');
-	//console.log('Hie');
-},2000);
--- resources/front-end server/Readme.md
+++
@@ -1,7 +0,0 @@
-#Travis-reporter
-A reporter to show the unstable Travis actions.
-
-#Installation
-```
-$ npm install
-```
