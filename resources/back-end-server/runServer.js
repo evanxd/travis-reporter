6ba4dfea46a380665c81be9f87597427c040d9b6
Sue-Yi-FAN <x95102003@yahoo.com.tw>
2014-05-14 21:47:02 -0700

    change folder name and create Makefile to run. modify back-end-server js file and let it could show some fake result

---
+++ resources/back-end-server/runServer.js
@@ -0,0 +1,12 @@
+
+/**
+ * Module dependencies.
+ */
+var express = require('express');
+var http = require('http');
+var insertData = require('./updateBase');
+var interval = require('./runTimer');
+var app = express();
+http.createServer(app).listen(app.get(4000), function(){
+  console.log('Express server listening on port ' + 4000);
+});
