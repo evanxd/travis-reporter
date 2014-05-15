d905ac19161c1fadb8b3bec3ebb16c51cce68232
Sue-Yi-FAN <x95102003@yahoo.com.tw>
2014-05-15 08:57:57 -0700

    change runServer

--- resources/back-end-server/runTimer.js
+++ resources/back-end-server/runTimer.js
@@ -1,5 +1,3 @@
-var fetch=require('./fetch-log')
-//var addr='https://s3.amazonaws.com/archive.travis-ci.org/jobs/23735955/log.txt';
+
 exports.inter = setInterval(function(){
-	fetch.lastBuildLog();
 },10000);
