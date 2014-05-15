a7d8424e256afee456301eb303289c51dc31f277
Sue-Yi-FAN <x95102003@yahoo.com.tw>
2014-05-15 09:17:10 -0700

    runTimer.js change

--- resources/back-end-server/runTimer.js
+++ resources/back-end-server/runTimer.js
@@ -1,6 +1,4 @@
 var fetch=require('./fetch-log')
-//var addr='https://s3.amazonaws.com/archive.travis-ci.org/jobs/23735955/log.txt';
 exports.inter = setInterval(function(){
-	//fetch.lastBuildLog();
 	console.log('it is timer run with ten second');
 },10000);
