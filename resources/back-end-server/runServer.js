fb6ee46625b4adbdc257411345f536a045baf719
Sue-Yi-FAN <x95102003@yahoo.com.tw>
2014-05-23 02:20:06 -0700

    change runServer to use port and modify updateBase.js

--- resources/back-end-server/runServer.js
+++ resources/back-end-server/runServer.js
@@ -5,7 +5,7 @@
 var express = require('express');
 var app = express();
 var http = require('http');
-// var insertData = require('./updateBase');
+var insertData = require('./updateBase');
 var interval = require('./runTimer');
 app.set("port",process.env.PORT||4000);
 var server = app.listen(app.get("port"),function(){
--- resources/back-end-server/updateBase.js
+++ resources/back-end-server/updateBase.js
@@ -10,23 +10,29 @@ var data = mongoose.model('datas',dataSchema);
 db.on('error',console.error.bind(console,'connection error:'));
 db.once('open',function callback(){
 	console.log('db link and update fake data done');
-	updateData('2013-12-11','find.js',5);
+	var json={"errDate":"2013/12/11","errName":"as.js","errCount":5};
+	update(json);
 });
-function updateData(date,name,count){
-	data.findOne({errName:name}).exec(function(err,ob){
-		if(err)
+function update(json){
+	data.findOne({errName:json.errName}).exec(function(err,ob){
+		if(err){
 			console.error(err);
+			insertData(json);
+		}
 		else{
-			ob.errCount+=count;
+			if(ob!=null){
+			ob.errCount+=json.errCount;
 			data.remove({errName:ob.errName})
 			.exec(insertData(ob));
+				}
+			else
+				insertData(json);
 			console.log(ob);
-		}
-	});
-}
+			}
+		});
+	}
 function insertData(json){
-
 	var test = new data({errDate:json.errDate,
 	errName:json.errName,errCount:json.errCount});
 	test.save();
-}
+	}
