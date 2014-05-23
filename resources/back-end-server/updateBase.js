22078dfe5348927fa91fe903c64fef0f4c2c07a2
Sue-Yi-FAN <x95102003@yahoo.com.tw>
2014-05-23 10:05:18 -0700

    correct the bug in the updataBase.js, can be use by call update method

--- resources/back-end-server/updateBase.js
+++ resources/back-end-server/updateBase.js
@@ -1,14 +1,15 @@
 var mongoose = require('mongoose');
 var db = mongoose.connection;
 var data = mongoose.model('data');
-db.on('error',console.error.bind(console,'connection error:'));
+/*db.on('error',console.error.bind(console,'connection error:'));
 db.once('open',function callback(){
 	console.log('db link and update fake data done');
-	var json={"date":"2013/12/11","fileName":"as.js","errCount":5};
+	var json={"date":"2013/12/4","fileName":"as.js","errCount":5};
 	update(json);
-});
-function update(json){
-	data.findOne({fileName:json.fileName}).exec(function(err,ob){
+});*/
+module.exports={
+update:function (json){
+	data.findOne({date:json.date,fileName:json.fileName}).exec(function(err,ob){
 		if(err){
 			console.error(err);
 			insertData(json);
@@ -16,17 +17,19 @@ function update(json){
 		else{
 			if(ob!=null){
 			ob.errCount+=json.errCount;
-			data.remove({fileName:ob.fileName})
+			data.remove({date:json.date,fileName:ob.fileName})
 			.exec(insertData(ob));
 			}
 			else
 				insertData(json);
-			console.log(ob);
 			}
 		});
 	}
+,
+}
 function insertData(json){
 	var test = new data({date:json.date,
 	fileName:json.fileName,errCount:json.errCount});
 	test.save();
-}
+	}
+
