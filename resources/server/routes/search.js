135230167e004c6c6f8a4f31e25f1ced28d73dd4
Chiene <sagiters1128@gmail.com>
2014-05-27 19:57:54 +0800

    Issue 167 - Bug - modify the search.js

--- resources/server/routes/search.js
+++ resources/server/routes/search.js
@@ -1,35 +1,31 @@
 var express = require('express');
-var mongoose= require('mongoose');
+var mongoose = require('mongoose');
 var router = express.Router();
 /*
  * @param {repoter} a Schema ,The it's structure is declared from the ./model/db.js document.
  */
-var repoter=mongoose.model('data');
-router.get('/', function(req, res) {
-	var	result = repoter.find({});
-		
-		if(req.query.fileName!=null)
-		{
-			result.find({fileName:req.query.fileName});
-		}
-		if(req.query.range!=null){
-			//result = repoter.find({})
-			var now=new Date();
-			var dayOfMonth = now.getDate();
-			now.setDate(dayOfMonth - req.query.range);
-			var test=now.getFulelYear()+"-"+(now.getMonth()<10 ? '0' : '')+(now.getMonth()+1)+"-"+ now.getDate();
-			result.where('date').gte(test);
-			result.sort('-date');
-			
-		}
-
-		result.limit(req.query.count);
-		result.sort('-errCount');
-		result.exec(function(err,result){	
-			res.send(result);
-			//console.dir(result);
-		});
+var repoter = mongoose.model('data');
+router.get('/',function(req, res) {
+	var result = repoter.find({});
+	result.sort('-errCount');  
+	if(req.query.fileName!=null)
+	{
+		result.find({fileName : req.query.fileName});
+	}
+	if(req.query.range!=null){
+		//result = repoter.find({})
+		var now = new Date();
+		var dayOfMonth = now.getDate();
+		now.setDate(dayOfMonth - req.query.range);
+		var range = now.getFullYear() + "-" +(now.getMonth()<10 ? '0' : '')+(now.getMonth()+1)+ "-" + now.getDate();
+		result.where('date').gte(range);
+	}
+	if(req.query.errCount!=null){
+		result.where('errCount').gte(req.query.errCount);	
+	}
+	result.limit(req.query.count);
+	result.exec(function(err,result){	
+		res.send(result);
+	});
 });
-
 module.exports = router;
-
