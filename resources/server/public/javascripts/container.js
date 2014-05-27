457fd1935166db25510a420cde4a9b7ab48cabec
John Wang <john29917958@gmail.com>
2014-05-28 00:22:12 +0800

    Issue 171 - Front-end code improvement.

--- resources/server/public/javascripts/container.js
+++ resources/server/public/javascripts/container.js
@@ -2,111 +2,40 @@
  * This class provides API to perform manipulation of test file bars
  * such as appending, detaching, and sorting test file bars on Travis-reporter.
  */
-define(function () {
+define(["dataBar"], function (DataBar) {
 	'use strict';
 	/**
 	 * @private Private variables.
 	 */
 
-	var large_to_small = true,
+	var large_to_small = true;
 
 	/**
-	 * Deciding:
-	 * type: current data sorting type.
-	 * option: what option data will sort by.
-	 */
-		sortConfigure = {
-			"type": large_to_small,
-			"option": null
-		},
-
-	/**
-	 * The target DOM object to be manipulated, the contains of manipulations
-	 * including such as appending and showing data, sorting data, and removing
-	 * data from it.
-	 */
-		target = null;
-
-	/**
-	 * @private This is a private function.
-	 * Swap two DOM which are children of target DOM.
-	 * @param {DOM} element01 The first DOM to be swaped.
-	 * @param {DOM} element02 The second DOM to be swaped.
-	 */
-	function dataSwap(element01, element02) {
-		$(element02).after($(element01).detach());
-	}
-
-	function sortingL2S (data, option) {
-		var hasChange = true,
-			value01,
-			value02,
-			isDigit = $.isNumeric($(data).children('.' + option).text()),
-			i = 0,
-			max = data.length-1,
-			temp = null;
-
-		while (hasChange) {
-			hasChange = false;
-			for (i = 0; i < max; i += 1) {
-				value01 = $(data[i]).children('.' + option).text().toLowerCase();
-				value02 = $(data[i+1]).children('.' + option).text().toLowerCase();
-
-				if (isDigit) {
-					value01 = Number(value01);
-					value02 = Number(value02);
-				}
-				
-				if(value01 < value02) {
-					dataSwap(data[i], data[i+1]);
-					hasChange = true;
-				}
-
-				temp = data[i];
-				data[i] = data[i+1];
-				data[i+1] = temp;
-			}
-		}
-	}
-
-	function sortingS2L (data, option) {
-		var hasChange = true,
-			value01,
-			value02,
-			isDigit = $.isNumeric($(data).children('.' + option).text()),
-			i = 0,
-			max = data.length-1,
-			temp = null;
-
-		while (hasChange) {
-			hasChange = false;
-			for (i = 0; i < max; i += 1) {
-				value01 = $(data[i]).children('.' + option).text().toLowerCase();
-				value02 = $(data[i+1]).children('.' + option).text().toLowerCase();
-
-				if (isDigit) {
-					value01 = Number(value01);
-					value02 = Number(value02);
-				}
-				
-				if(value01 > value02) {
-					dataSwap(data[i], data[i+1]);
-					hasChange = true;
-				}
-
-				temp = data[i];
-				data[i] = data[i+1];
-				data[i+1] = temp;
-			}
-		}
-	}
-
-	/**
+	 * @class Container
 	 * @constructor
 	 * @param {DOM} target The target DOM which the test bar to be appended to.
 	 */
 	function Container (inputTarget) {
-		target = inputTarget;
+		/**
+		 * The target DOM object to be manipulated, the manipulations includes
+		 * appending and showing data, sorting data, and removing data from it.
+		 */
+		this.target = inputTarget;
+
+		/**
+		 * The dataBars to be shown on Travis-reporter.
+		 */
+		this.dataBars = [];
+
+		/**
+		 * Deciding:
+		 * type: current data sorting type.
+		 * option: what option data will sort by.
+		 */
+		this.sortConfigure = {
+			"type": large_to_small,
+			"option": null
+		};
 	}
 
 	/**
@@ -115,46 +44,61 @@ define(function () {
 	Container.prototype = {		
 		/**
 		 * Append data to Travis-reporter as a bar.
-		 *
 		 * @param {JSON} data The data to be appended to target DOM.
 		 */
 		appendData: function (data) {
 			var i,
 				max = data.length,
-				bar = null,
-				btDetail = null,
+				$bar = null,
+				$column = null,
+				$btDetail = null,
 				key = null;
 
-			for(i = 0; i < max; i += 1) {
-				bar = '<tr id="info_bar_no' + data[i].id + '" class="tb_info_bar">';
-				btDetail = '<button id="bt_detail_no' + data[i].id + '" class="bt_detail">Detail</button>';
+			for (i = 0; i < max; i += 1) {
+				$bar = $("<tr>");
+				$bar.attr("id", "info_bar_no" + data[i].id);
+				$bar.attr("class", "tb_info_bar");
 
-				for(key in data[i]) {
+				$btDetail = $("<button>");
+				$btDetail.attr("id", "bt_detail_no" + data[i].id);
+				$btDetail.attr("class", "bt_detail");
+				$btDetail.append("Detail");
+
+				for (key in data[i]) {
 					if(key !== 'id') {
-						bar = bar + '<td class="' + key + '">' + data[i][key] + '</td>';
+						$column = $("<td>");
+						$column.attr("class", key);
+						$column.append(data[i][key]);
+						$bar.append($column);
 					}
 				}
 			
-				bar = bar + '<td class="tb_last">' + btDetail + '</td>';
-				bar = bar + '</tr>';
+				$column = $("<td>");
+				$column.attr("class", "tb_last");
+				$column.append($btDetail);
 			
-				$(target).append(bar);
+				$bar.append($column);
+
+				$(this.target).append($bar);
+	
+				this.dataBars.push(new DataBar($("#info_bar_no" + data[i].id)));
 			}
 		},
 
+		/**
+		 * Get the specific data bar with the given value.
+		 * @param {String} name The certain column of the data bar.
+		 * @param {String} value The corresponding value of the column.
+		 * @returns {DOM} A whole data bar DOM.
+		 */
 		getData: function (name, value) {
-			var data = $('tr.tb_info_bar').children('td.' + name).find(':contains("' + value + '")'),
-				columns = null,
-				result = [],
-				i, j,
-				max = data.length,
-				max2 = null;
+			var result = [],
+				i,
+				max = this.dataBars.length;
 
-			for(i = 0; i < max; i += 1) {
-				columns = $(data[i]).children();
-				max2 = columns.length;
-				for(j = 0; j < max2; j += 1) {
-					result[i][$(columns[j]).attr('class')] = $(columns[j]).text();
+			for (i = 0; i < max; i += 1) {
+				if (this.dataBars[i].getInfo(name) === value) {
+					result.push(this.dataBars[i].get());
 				}
 			}
 
@@ -163,42 +107,117 @@ define(function () {
 		
 		/**
 		 * Sort data which are children of target DOM by specific option.
-		 *
 		 * @param {String} option Data will sorted by this argument.
-		 * @return {Boolean} True for sorting data from large to small, false otherwise.
+		 * @returns {Boolean} True for sorting data from large to small, false otherwise.
 		 */
 		sort: function (option) {
-			if (option === undefined && sortConfigure.option === undefined) {
+			var i, max;
+
+			if (option === undefined && this.sortConfigure.option === undefined) {
 				console.log('container.js function sort() error.');
 			}
 			else {
-				//Configuring the sorting type and option.
-				if (sortConfigure.option === option) {
-					sortConfigure.type = !sortConfigure.type;
+				// Configuring the sorting type and option.
+				if (this.sortConfigure.option === option) {
+					this.sortConfigure.type = !this.sortConfigure.type;
 				}
 				else {
-					sortConfigure.option = option;
-					sortConfigure.type = large_to_small;
+					this.sortConfigure.option = option;
+					this.sortConfigure.type = large_to_small;
 				}
-		
-				if (sortConfigure.type === large_to_small) {
-					sortingL2S($('.tb_info_bar'), option);
+				
+				// Choose sorting function and execute it.
+				if (this.sortConfigure.type === large_to_small) {
+					this.sortingL2S();
 				}
 				else {
-					sortingS2L($('.tb_info_bar'), option);
+					this.sortingS2L(this.dataBars, option);
 				}
 			}
 
-			return sortConfigure.type;
+			// Actions after sorting.
+			// Clear data in target container.
+			$('.tb_info_bar').detach();
+
+			// Then add sorted data on to target container.
+			max = this.dataBars.length;
+			for (i = 0; i < max; i += 1) {
+				$(this.target).append(this.dataBars[i].get());
+			}
+
+			return this.sortConfigure.type;
+		},
+
+		sortingL2S: function () {
+			var hasChange = true,
+				value01,
+				value02,
+				i = 0,
+				max = this.dataBars.length-1;
+
+			while (hasChange) {
+				hasChange = false;
+				for (i = 0; i < max; i += 1) {
+					value01 = this.dataBars[i].getInfo(this.sortConfigure.option);
+					value02 = this.dataBars[i + 1].getInfo(this.sortConfigure.option);
+					
+					if($.isNumeric(value01)) {
+						value01 = Number(value01);
+						value02 = Number(value02);
+					}
+					else {
+						value01 = value01.toLowerCase();
+						value02 = value02.toLowerCase();
+					}
+
+					if(value01 < value02) {
+						this.dataSwap(i, i + 1);
+						hasChange = true;
+					}
+				}
+			}
+		},
+
+		sortingS2L: function () {
+			var hasChange = true,
+				value01,
+				value02,
+				i = 0,
+				max = this.dataBars.length-1;
+
+			while (hasChange) {
+				hasChange = false;
+				for (i = 0; i < max; i += 1) {
+					value01 = this.dataBars[i].getInfo(this.sortConfigure.option);
+					value02 = this.dataBars[i + 1].getInfo(this.sortConfigure.option);
+					
+					if($.isNumeric(value01)) {
+						value01 = Number(value01);
+						value02 = Number(value02);
+					}
+					else {
+						value01 = value01.toLowerCase();
+						value02 = value02.toLowerCase();
+					}
+
+					if(value01 > value02) {
+						this.dataSwap(i, i + 1);
+						hasChange = true;
+					}
+				}
+			}
 		},
 		
 		/**
-		 * Swap two DOM which are children of target DOM.
-		 * @param {DOM} element01 The first DOM to be swaped.
-		 * @param {DOM} element02 The second DOM to be swaped.
+		 * Swap two data bars in this object but not actually swap two
+		 * DOM data bar on the target container.
+		 * @param {Integer} index01 First data bar to be swaped.
+		 * @param {Integer} index02 Second data bar to be swaped.
 		 */
-		dataSwap: function (element01, element02) {
-			$(element02).after($(element01).detach());
+		dataSwap: function (index01, index02) {
+			var temp = this.dataBars[index01];
+			this.dataBars[index01] = this.dataBars[index02];
+			this.dataBars[index02] = temp;
 		},
 
 		/**
@@ -206,6 +225,7 @@ define(function () {
 		 */
 		clear: function () {
 			$('.tb_info_bar').detach();
+			this.dataBars = [];
 		}
 	};
 
