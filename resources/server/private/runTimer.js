56a5721513a9853e4acd3b9ac18fae8703aa1580
Sue-Yi-FAN <x95102003@yahoo.com.tw>
2014-05-27 08:29:27 -0700

    modify the timer we fetch

--- resources/server/private/runTimer.js
+++ resources/server/private/runTimer.js
@@ -2,7 +2,7 @@ var fetch=require('./fetch');
 var buildTime="2014-05-26T17:24:50Z";
 exports.inter = setInterval(function(){
 	fetch.doThing(buildTime,setTime);
-},10000);
+},300000);
 function setTime(time){
 	buildTime=time;
 }
