bb0085c0c56e2fcece78082dc0a380636cf82ea1
Sue-Yi-FAN <x95102003@yahoo.com.tw>
2014-05-30 07:32:40 -0700

    modify the parse data format

--- resources/server/private/parser.js
+++ resources/server/private/parser.js
@@ -1,10 +1,20 @@
-
 module.exports = {
 	findErrFile : function(data,callback){
-		var patt=/(\w)*\.js/g;
+		/*var patt=/gaia(\w)*\.js/g;
 		var spiltStr=data.substr(data.indexOf("failing"));
         spiltStr=spiltStr.substr(0,spiltStr.indexOf("make"));
        	var result =spiltStr.match(patt);
-        return result;
+        return result;*/
+        var patt=/gaia\/.*_test\.js/g;
+		var result=temp.match(patt);
+		var json ={"Path":{},"Name":{}};
+		for(var i=0;i<result.length;i++){
+			console.log(result[i]);
+			var name = result[i].substring(result[i].lastIndexOf("/")+1,result[i].length);
+			var path = result[i].substring(result[i].indexOf("/"),result[i].indexOf(name));
+			json.Path[i]=path;
+			json.Name[i]=name;
+		}
+		return json;
 	}
 }
