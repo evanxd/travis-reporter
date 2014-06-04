7fdc78839fd6902af6f84d8fc110c1775e164aea
Sue-Yi-FAN <x95102003@yahoo.com.tw>
2014-06-04 04:18:05 -0700

    modify db schema and parser.js

--- resources/private/parser.js
+++ resources/private/parser.js
@@ -1,21 +1,13 @@
 module.exports = {
 	findErrFile : function(data,callback){
-		/*var patt=/gaia(\w)*\.js/g;
-		var spiltStr=data.substr(data.indexOf("failing"));
-        spiltStr=spiltStr.substr(0,spiltStr.indexOf("make"));
-       	var result =spiltStr.match(patt);
-        return result;*/
+        var temp=data.substr(data.indexOf("failing"),data.lastIndexOf("make"));
         var patt=/gaia\/.*_test\.js/g;
-		var result=data.match(patt);
+		var result=temp.match(patt);
 		if(result==null)
 			return null;
-		var json ={"Path":{},"Name":{},"length":result.length};
 		for(var i=0;i<result.length;i++){
-			var name = result[i].substring(result[i].lastIndexOf("/")+1,result[i].length);
-			var path = result[i].substring(result[i].indexOf("/"),result[i].indexOf(name));
-			json.Path[i]=path;
-			json.Name[i]=name;
+			result[i]=result[i].substring(result[i].indexOf("/"),result[i].length);
 		}
-		return json;
+		return result;
 	}
 }
